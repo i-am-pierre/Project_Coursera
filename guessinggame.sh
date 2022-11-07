418763abd91d851e2ca539641930fda61ba103f6
i-am-pierre <ervais.pierre@mac.com>
2022-11-07 18:03:44 +0800

    basic program done without line count

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,23 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 # File: guessinggame.sh
 
+nbfiles=5
+
+function strict_compare {
+if [[ $1 -gt $2 ]]
+  then
+  echo "Too High, try again"
+  else 
+  echo "Too Low, try again"
+fi
+}
+
+echo "Guess the Number of files in the current directory"
+read guess
+
+while [[ $guess -ne $nbfiles ]]
+do
+  strict_compare $guess $nbfiles
+  read guess
+done
+echo "Congratulations"
