5b975100770f4351dbcce8c64b7caa7a3db55689
i-am-pierre <ervais.pierre@mac.com>
2022-11-07 18:29:35 +0800

    Fix Typo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Project Coursera Week 4: Guessing Game #
 Created Date: 
-Mon Nov  7 18:14:37 +08 2022
+Mon Nov  7 18:29:02 +08 2022
 Number of lines: 
-23
+25
--- guessinggame.sh
+++ guessinggame.sh
@@ -6,18 +6,20 @@ nbfiles=$(find . -maxdepth 1 -type f | wc -l)
 function strict_compare {
 if [[ $1 -gt $2 ]]
   then
-  echo "Too High, try again"
+  echo "Too high"
   else 
-  echo "Too Low, try again"
+  echo "Too low"
 fi
 }
 
-echo "Guess the Number of files in the current directory"
+echo "Guess the Number of files in the current directory?"
+echo "Make a guess: "
 read guess
 
 while [[ $guess -ne $nbfiles ]]
 do
   strict_compare $guess $nbfiles
+  echo "Make a better guess:"
   read guess
 done
 echo "Congratulations"
